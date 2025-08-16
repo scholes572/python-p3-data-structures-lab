b7bacb1a3bb07325c233cd50551268d3c6b1f31e
Agostino <agostinoscholes572@gmail.com>
2025-08-16 15:03:13 +0300

    made changes

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -20,28 +20,26 @@ def get_names(spicy_foods):
     return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    return [food for food in spicy_foods if food["heat_level"] > 5 ]
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
-        print(f"{food["name"]} ({food['cuisine']}) | Heat Level: { '🌶' * food['heat_level']}")
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food["cuisine"] == cuisine:
             return food
     return None    
-    
 
 def print_spiciest_foods(spicy_foods):
     spiciest = get_spiciest_foods(spicy_foods)
     for food in spiciest:
-        print (f"{food["name"]} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
-
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
 def get_average_heat_level(spicy_foods):
     total_heat = sum(food["heat_level"] for food in spicy_foods)
-    return total_heat // len(spicy_foods)
+    return int(total_heat / len(spicy_foods))
 
 def create_spicy_food(spicy_foods, spicy_food):
     spicy_foods.append(spicy_food)
